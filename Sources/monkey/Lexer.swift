e3188e7fb3b2f7c67533981d5cae52f16db0445b
swiftty <62803132+swiftty@users.noreply.github.com>
2020-03-29 04:22:03 +0900

    use TextScanner to implement Lexer

--- Sources/monkey/Lexer.swift
+++ Sources/monkey/Lexer.swift
@@ -1,26 +1,16 @@
+import TextScanner
 
 public struct Lexer {
-    public var input: String
-    public var position: Int = 0
-    public var readPosition: Int = 0
-
-    public var ch: Character?
+    private var scanner: TextScanner
+    public private(set) var ch: Character?
 
     public init(_ input: String) {
-        self.input = input
+        scanner = TextScanner(input)
         readChar()
     }
 
     public mutating func readChar() {
-        let index = input.index(input.startIndex, offsetBy: readPosition, limitedBy: input.endIndex)
-            ?? input.endIndex
-        if input.indices.contains(index) {
-            ch = input[index]
-        } else {
-            ch = nil
-        }
-        position = readPosition
-        readPosition += 1
+        ch = scanner.next()
     }
 
     public mutating func nextToken() -> Token? {
