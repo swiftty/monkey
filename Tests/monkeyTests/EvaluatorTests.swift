3dd94f1371dc947cb94980ed55563e617e41deaa
swiftty <62803132+swiftty@users.noreply.github.com>
2020-04-15 23:58:10 +0900

    introduce ExpressionInput

--- Tests/monkeyTests/EvaluatorTests.swift
+++ Tests/monkeyTests/EvaluatorTests.swift
@@ -2,59 +2,59 @@ import XCTest
 @testable import monkey
 
 final class EvaluatorTests: XCTestCase {
-    func testEvalExpression() throws {
-        let tests: [(input: String, expected: Any?)] = [
-            ("5", 5 as Int64),
-            ("10", 10 as Int64),
-            ("true", true),
-            ("false", false),
-            ("!true", false),
-            ("!false", true),
-            ("!5", false),
-            ("!!true", true),
-            ("!!false", false),
-            ("!!5", true),
-            ("1 < 2", true),
-            ("1 > 2", false),
-            ("1 < 1", false),
-            ("1 > 1", false),
-            ("1 == 1", true),
-            ("1 != 1", false),
-            ("1 == 2", false),
-            ("1 != 2", true),
-            ("true == true", true),
-            ("false == false", true),
-            ("true == false", false),
-            ("true != false", true),
-            ("false != true", true),
-            ("(1 < 2) == true", true),
-            ("(1 < 2) == false", false),
-            ("(1 > 2) == true", false),
-            ("(1 > 2) == false", true),
-            ("-5", -5 as Int64),
-            ("-10", -10 as Int64),
-            ("5 + 5 + 5 + 5 - 10", 10 as Int64),
-            ("5 + 5 + 5 + 5 - 30", -10 as Int64),
-            ("2 * 2 * 2 * 2 * 2", 32 as Int64),
-            ("-50 + 100 + -50", 0 as Int64),
-            ("5 * 2 + 10", 20 as Int64),
-            ("5 + 2 * 10", 25 as Int64),
-            ("20 + 2 * -10", 0 as Int64),
-            ("50 / 2 * 2 + 10", 60 as Int64),
-            ("2 * (5 + 10)", 30 as Int64),
-            ("3 * 3 * 3 + 10", 37 as Int64),
-            ("3 * (3 * 3) + 10", 37 as Int64),
-            ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50 as Int64),
+    func testEvalExpression() {
+        let tests: [ExpressionInput<Any?>] = [
+            .init("5", 5 as Int64),
+            .init("10", 10 as Int64),
+            .init("true", true),
+            .init("false", false),
+            .init("!true", false),
+            .init("!false", true),
+            .init("!5", false),
+            .init("!!true", true),
+            .init("!!false", false),
+            .init("!!5", true),
+            .init("1 < 2", true),
+            .init("1 > 2", false),
+            .init("1 < 1", false),
+            .init("1 > 1", false),
+            .init("1 == 1", true),
+            .init("1 != 1", false),
+            .init("1 == 2", false),
+            .init("1 != 2", true),
+            .init("true == true", true),
+            .init("false == false", true),
+            .init("true == false", false),
+            .init("true != false", true),
+            .init("false != true", true),
+            .init("(1 < 2) == true", true),
+            .init("(1 < 2) == false", false),
+            .init("(1 > 2) == true", false),
+            .init("(1 > 2) == false", true),
+            .init("-5", -5 as Int64),
+            .init("-10", -10 as Int64),
+            .init("5 + 5 + 5 + 5 - 10", 10 as Int64),
+            .init("5 + 5 + 5 + 5 - 30", -10 as Int64),
+            .init("2 * 2 * 2 * 2 * 2", 32 as Int64),
+            .init("-50 + 100 + -50", 0 as Int64),
+            .init("5 * 2 + 10", 20 as Int64),
+            .init("5 + 2 * 10", 25 as Int64),
+            .init("20 + 2 * -10", 0 as Int64),
+            .init("50 / 2 * 2 + 10", 60 as Int64),
+            .init("2 * (5 + 10)", 30 as Int64),
+            .init("3 * 3 * 3 + 10", 37 as Int64),
+            .init("3 * (3 * 3) + 10", 37 as Int64),
+            .init("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50 as Int64),
         ]
 
         for t in tests {
             let evaluated = _eval(t.input)
             switch t.expected {
             case let expected as Int64:
-                try checkIntegerObject(evaluated, expected: expected)
+                XCTAssertNoThrow(try checkIntegerObject(evaluated, expected: expected, file: t.file, line: t.line))
 
             case let expected as Bool:
-                try checkBooleanObject(evaluated, expected: expected)
+                XCTAssertNoThrow(try checkBooleanObject(evaluated, expected: expected, file: t.file, line: t.line))
 
             case nil:
                 XCTAssert(evaluated is Null)
---
+++ Tests/monkeyTests/XCTestCase+Helper.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+struct ExpressionInput<Expected> {
+    let input: String
+    let expected: Expected
+    let file: StaticString
+    let line: UInt
+
+    init(_ input: String, _ expected: Expected, file: StaticString = #file, line: UInt = #line) {
+        self.input = input
+        self.expected = expected
+        self.file = file
+        self.line = line
+    }
+}
